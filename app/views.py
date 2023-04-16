801120bbfbb747c9ceb21824664bd9d9bde24bc7
pratyusha319 <pratyushagorrela8@gmail.com>
2023-04-16 10:35:23 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -10,29 +10,29 @@ def insert_topic(request):
         return HttpResponse('Topic insertion is done successfully')
     return render(request,'insert_topic.html')
 def insert_webpage(request):
+    LTO=Topic.objects.all()
+    d={'topics':LTO}
     if request.method=='POST':
         tn=request.POST['tn']
-        TO=Topic.objects.get_or_create(topic_name=tn)[0]
-        TO.save()
-        na=request.POST['na']
+        na=request.POST.get['na']
         ur=request.POST['ur']
         em=request.POST['em']
+        TO=Topic.objects.get(topic_name=tn)
 
         WO=Webpage.objects.get_or_create(topic_name=TO,name=na,url=ur,email=em)[0]
         WO.save()
         return HttpResponse('webpage insertion is done successfully')
-    return render(request,'insert_webpage.html')
+    return render(request,'insert_webpage.html',d)
 def insert_accessrecords(request):
+    LWO=Webpage.objects.all()
+    d={'webpages':LWO}
     if request.method=='POST':
-       
         na=request.POST['na']
-        WO=Webpage.objects.get_or_create(name=na)[0]
-        WO.save()
         au=request.POST['au']
         da=request.POST['da']
-
+        WO=Webpage.objects.get(name=na)
        
         AO=AccessRecord.objects.get_or_create(name=WO,author=au,date=da)[0]
         AO.save()
-        return HttpResponse('webpage insertion is done successfully')
-    return render(request,'insert_accessrecords.html')
+        return HttpResponse('accessrecord insertion is done successfully')
+    return render(request,'insert_accessrecords.html',d)
